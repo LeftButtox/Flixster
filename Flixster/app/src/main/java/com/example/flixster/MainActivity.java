2b0194fa52fef0b877272f2d6c35aaf892dff063
LeftButtox <alexanvl@uci.edu>
2021-01-10 13:53:40 -0800

    initial commit, finsihed part 1 of flixster project

---
+++ Flixster/app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.adapters.MovieAdapter;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+        // Create the adapter
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        // Set the adapter on the recycler view
+        rvMovies.setAdapter(movieAdapter);
+
+        // Set a Layout Manager on the recycler view
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        // no need to make a RequestParams because api key is in url
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() { // json because the movie database api returns json
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess called");
+                JSONObject jsonObject = json.jsonObject; // returns json object
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results"); // there may not be key called results
+                    Log.i(TAG, "Results: "+ results.toString()); // Log.i displays information
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged(); // refresh adapter
+                    Log.i(TAG, "Movies: "+ movies.size());
+                } catch (JSONException e) {
+                    Log.d(TAG, "JSON EXCEPTION", e); // Log.d displays debug
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure called");
+            }
+        });
+
+
+
+
+    }
+}
